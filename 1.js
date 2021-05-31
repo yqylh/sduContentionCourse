94f8291ea68f96ff758c6d30d890eb9bb8ed3ce8
yqy <1071665725@qq.com>
2021-05-31 17:12:31 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,46 +1,109 @@
-var kch = [];
-var kxh = [];
-var kcpage = [];
-var kcpagerank = [];
-var pagenum = 0;
-var firstDo, secondDo;
-var num = 0;
-function trySelect() {
-	var pushsearch = "type=kc&currentPage=" + kcpage[num] + "&kch=&jsh=&skxq=&skjc=&kkxsh="
-	MyApp.post("/b/xk/xs/kcsearch", pushsearch, function(data) {
-		var there = data.object.resultList[kcpagerank[num]];
-		num = num + 1;
-		if (num == kcpage.length) num = 0;
-		if (there.kyl > 0) {
-			var url = "/b/xk/xs/add/" + there.KCH + "/" + there.KXH;
-			MyApp.post(url, function(data) {
-// 				console.log(data.msg)
-				alert(data.msg)
-			});
-		} else {
-			console.log(there.KCM + "课程号：" + there.KCH + "课序号：" + there.KXH + "  抱歉无课\n")
-		}
-	})
+// 在选课页面F12-console 复制粘贴运行
+// 仅支持必修课 选修课内能查询到的课 , 辅修和重修请自行选课
+// 请按照下面两行的格式填入课程号和课序号
+let kch = ["SQ0000459H", "SQ0000411H"];
+let kxh = ["400", "400"];
+let kchid = [];
+let kxhid = [];
+let reqData = [
+    { "name": "sEcho", "value": "1" },
+    { "name": "iColumns", "value": "1" },
+    { "name": "sColumns", "value": "" },
+    { "name": "iDisplayStart", "value": "0" },
+    { "name": "iDisplayLength", "value": "1" },
+    { "name": "mDataProp_0", "value": "kch" },
+    { "name": "mDataProp_1", "value": "kcmc" },
+    { "name": "mDataProp_2", "value": "kxhnew" },
+    { "name": "mDataProp_3", "value": "jkfs" },
+    { "name": "mDataProp_4", "value": "xmmc" },
+    { "name": "mDataProp_5", "value": "xf" },
+    { "name": "mDataProp_6", "value": "skls" },
+    { "name": "mDataProp_7", "value": "sksj" },
+    { "name": "mDataProp_8", "value": "skdd" },
+    { "name": "mDataProp_9", "value": "xqmc" },
+    { "name": "mDataProp_10", "value": "xkrs" },
+    { "name": "mDataProp_11", "value": "syrs" },
+    { "name": "mDataProp_12", "value": "ctsm" },
+    { "name": "mDataProp_13", "value": "szkcflmc" },
+    { "name": "mDataProp_14", "value": "czOper"}
+];
+var RXdata, BXdata, AllData;
+let searchRX = ()=>{
+    return new Promise((resolve, reject)=>{
+        $.ajax({
+            type:"post",
+            url:"/jsxsd/xsxkkc/xsxkGgxxkxk?kcxx=&skls=&skxq=&skjc=&sfym=false&sfct=true&szjylb=&sfxx=false&skfs=",
+            data:reqData,
+            success:function(resp){ 
+                RXdata = $.parseJSON(resp);
+                resolve();
+            }
+        });
+    });
 }
-function getwz() {
-	var pushsearch = "type=kc&currentPage=" + pagenum + "&kch=&jsh=&skxq=&skjc=&kkxsh="
-	pagenum = pagenum + 1;
-	MyApp.post("/b/xk/xs/kcsearch", pushsearch, function(data) {
-		var testlist = data.object.resultList;
-		for (var j = 0; j < data.object.resultList.length; j++) {
-            for (var k = 0; k < kch.length; k++) {
-                if (testlist[j].KCH == kch[k] && testlist[j].KXH == kxh[k]) {
-                    kcpage.push(data.object.currentPage)
-                    kcpagerank.push(j)
-                    console.log(data.object.currentPage, j)
-                }
+let searchBX = ()=>{
+    return new Promise((resolve, reject)=>{
+        $.ajax({
+            type:"post",
+            url:"/jsxsd/xsxkkc/xsxkBxxk?1=1&kcxx=&skls=&skfs=",
+            data:reqData,
+            success:function(resp){ 
+                BXdata = $.parseJSON(resp);
+                resolve();
+            }
+        });
+    });
+}
+function xsxkOper(kchid,kxhid){
+    return new Promise((resolve,reject)=>{
+        var param = "?kcid="+kxhid+"&cfbs=null";
+        $.ajax({
+            url:"/jsxsd/xsxkkc/ggxxkxkOper"+param,
+            data:{
+                jx0404id:kchid,
+                xkzy:"",
+                trjf:""
+            },
+            success:function(resp){ 
+                console.log(resp);
+                resolve();
+            }
+        })
+    })
+}
+let main = async function() {
+    await searchRX();
+    let allNum = RXdata.iTotalRecords;
+    reqData[1].value = allNum;
+    reqData[4].value = allNum;
+    await searchRX();
+    //
+    reqData[1].value = 1;
+    reqData[4].value = 1;
+    //
+    await searchBX();
+    allNum = BXdata.iTotalRecords;
+    reqData[1].value = allNum;
+    reqData[4].value = allNum;
+    await searchBX();
+    for (let i = 0; i < kch.length; i++) {
+        for (let j of BXdata.aaData) {
+            if (j.kch == kch[i] && j.kxh == kxh[i]) {
+                kchid.push(j.jx0404id);
+                kxhid.push(j.jx02id);
             }
         }
-		if (data.object.currentPage == data.object.totalPages) {
-            clearInterval(firstDo);
-            console.log('读取完毕');
-            secondDo = setInterval(trySelect, 300)
+        for (let j of RXdata.aaData) {
+            if (j.kch == kch[i] && j.kxh == kxh[i]) {
+                kchid.push(j.jx0404id);
+                kxhid.push(j.jx02id);
+            }
         }
-	})
+    }
+    setInterval(() =>{
+        for (let i = 0; i < kchid.length; i++) {
+            xsxkOper(kchid[i],kxhid[i])
+        }
+    }, 1000);
 }
-firstDo = setInterval(getwz, 300)
+main();
